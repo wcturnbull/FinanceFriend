8bc5ec91b09eee2651b3f24eb7236119ff825d45
HenryWellman <hdwellma@purdue.edu>
2023-09-24 13:37:09 -0400

    Added Basic Form Generation
    Made the add investments button and lets you select "stock" and price, as well as the amount and displays it in a form

--- lib/investment_page.dart
+++ lib/investment_page.dart
@@ -1,16 +1,110 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 
-class InvestmentPage extends StatelessWidget {
+class InvestmentPage extends StatefulWidget {
+  @override
+  _InvestmentPageState createState() => _InvestmentPageState();
+}
+
+class _InvestmentPageState extends State<InvestmentPage> {
+  final GlobalKey<FormBuilderState> _fbKey = GlobalKey<FormBuilderState>();
+  List<Map<String, dynamic>> investments = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Investment Page'),
       ),
-      body: Center(
-        child: Text(
-          'This is the Investment Page',
-          style: TextStyle(fontSize: 24),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text('Add Investment'),
+                        content: FormBuilder(
+                          key: _fbKey,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          child: Column(
+                            children: [
+                              FormBuilderDropdown(
+                                name: 'investmentOption',
+                                items: List.generate(
+                                  10,
+                                  (index) => DropdownMenuItem(
+                                    value: (index + 1).toString(),
+                                    child: Text((index + 1).toString()),
+                                  ),
+                                ),
+                              ),
+                              FormBuilderTextField(
+                                name: 'price',
+                                decoration: InputDecoration(labelText: 'Price'),
+                                keyboardType: TextInputType.number,
+                              ),
+                              FormBuilderTextField(
+                                name: 'amount',
+                                decoration:
+                                    InputDecoration(labelText: 'Amount'),
+                                keyboardType: TextInputType.number,
+                              ),
+                            ],
+                          ),
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              if (_fbKey.currentState!.saveAndValidate()) {
+                                final formData = _fbKey.currentState!.value;
+                                setState(() {
+                                  investments.add({
+                                    'Option': formData['investmentOption'],
+                                    'Price': formData['price'],
+                                    'Amount': formData['amount'],
+                                  });
+                                });
+                                Navigator.of(context).pop();
+                              }
+                            },
+                            child: Text('Save'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('Cancel'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: Text('Add Investments'),
+              ),
+              SizedBox(height: 20),
+              if (investments.isNotEmpty)
+                DataTable(
+                  columns: [
+                    DataColumn(label: Text('Option')),
+                    DataColumn(label: Text('Price')),
+                    DataColumn(label: Text('Amount')),
+                  ],
+                  rows: investments.map((investment) {
+                    return DataRow(cells: [
+                      DataCell(Text(investment['Option'])),
+                      DataCell(Text(investment['Price'])),
+                      DataCell(Text(investment['Amount'])),
+                    ]);
+                  }).toList(),
+                ),
+            ],
+          ),
         ),
       ),
     );
