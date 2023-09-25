dea9ad34777abd51c9ca47fd8ad07b87fc1ca46d
Patrick Spitzer <pspitzer20@gmail.com>
2023-09-25 17:11:51 -0400

    Added button and popup for bill input

--- lib/tracking.dart
+++ lib/tracking.dart
@@ -11,6 +11,11 @@ class TrackingPage extends StatefulWidget {
 }
 
 class _TrackingPageState extends State<TrackingPage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController billTitleController = TextEditingController();
+  final TextEditingController billDataController = TextEditingController();
+  final TextEditingController billDateController = TextEditingController();
+
   void _navigateToHomePage(BuildContext context) {
     Navigator.of(context).push(MaterialPageRoute(
       builder: (context) => MyHomePage(title: widget.title),
@@ -110,6 +115,100 @@ class _TrackingPageState extends State<TrackingPage> {
                 }
               },
             ),
+            ElevatedButton(
+              onPressed: () async {
+                await showDialog<void>(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    content: Stack(
+                      children: <Widget>[
+                            Positioned(
+                              right: -40,
+                              top: -40,
+                              child: InkResponse(
+                                onTap: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const CircleAvatar(
+                                  backgroundColor: Colors.red,
+                                  child: Icon(Icons.close),
+                                ),
+                              ),
+                            ),
+                            Form(
+                              key: _formKey,
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: <Widget>[
+                                  Padding(
+                                    padding: const EdgeInsets.all(8),
+                                    child: Text('Please input bill data that you would like us to keep track of.', style: TextStyle(fontSize: 20))
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8),
+                                    child: Row(children: [
+                                      Text('Bill Title: ', style: TextStyle(fontSize: 14)),
+                                      Expanded(
+                                        child: TextFormField(
+                                          decoration: InputDecoration(
+                                            hintText: 'Enter a title to identify the bill',
+                                          ),
+                                          controller: billTitleController,
+                                        ),
+                                      ),
+                                    ],)
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8),
+                                    child: Row(children: [
+                                      Text('Bill Data: ', style: TextStyle(fontSize: 14)),
+                                      Expanded(
+                                        child: TextFormField(
+                                          decoration: InputDecoration(
+                                            hintText: "Enter some data that you'd like to remember about the bill",
+                                          ),
+                                          controller: billDataController,
+                                        ),
+                                      ),
+                                    ],)
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8),
+                                    child: Row(children: [
+                                      Text('Bill Due Date: ', style: TextStyle(fontSize: 14)),
+                                      Expanded(
+                                        child: TextFormField(
+                                          decoration: InputDecoration(
+                                            hintText: 'Use MM/DD/YYYY',
+                                          ),
+                                          controller: billDateController,
+                                        ),
+                                      ),
+                                    ],)
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8),
+                                    child: ElevatedButton(
+                                      child: const Text('Submit'),
+                                      onPressed: () {
+                                        String billTitle = billTitleController.text;
+                                        String billData = billDataController.text;
+                                        String billDate = billDateController.text;
+                                        //send the data to database, refresh page if necessary
+                                        Navigator.of(context).pop();
+                                      },
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                    )
+                  )
+                );
+              },
+              child: const Text('Open Popup'),
+            )
           ],)
       ),
     );
