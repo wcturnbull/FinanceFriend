01eb95c6883d91c2c7889e67b6ebb824ba12b6b7
Wes Turnbull <wturnbul@purdue.edu>
2023-09-26 22:39:29 -0400

    Updated graph_page to update with an input

--- lib/graph_page.dart
+++ lib/graph_page.dart
@@ -7,16 +7,35 @@ class GraphPage extends StatefulWidget {
 }
 
 class _GraphPageState extends State<GraphPage> {
-  Map<String, double> dataMap1 = {
-    "Tuition and Fees": 40,
-    "Housing and Utilities": 25,
-    "Food and Groceries": 15,
-    "Transportation": 5,
-    "Textbooks and Supplies": 5,
-    "Entertainment and Leisure": 5,
-    "Healthcare": 3,
-    "Savings and Emergency Fund": 2
+  late TextEditingController controller;
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  String actualCategory = "";
+  bool values_added = false;
+
+  String selectedCategory = "Select Category";
+  bool isFormValid = false; // Initialize with a default category
+
+  Map<String, double> budgetMap = {
+    "Housing": 0,
+    "Utilities": 0,
+    "Food": 0,
+    "Transportation": 0,
+    "Entertainment": 0,
+    "Investments": 0,
+    "Debt Payments": 0
   };
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   final Color color =
       Color(int.parse("#248712".substring(1, 7), radix: 16) + 0xFF0000000);
@@ -32,25 +51,45 @@ class _GraphPageState extends State<GraphPage> {
     Color(int.parse("#A0E75A".substring(1, 7), radix: 16) + 0xFF0000000),
   ];
 
-  // GraphPage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
       appBar: AppBar(
         title: const Text('Graph Page'),
       ),
-      body: Center(
-        child: PieChart(
-          dataMap: dataMap1,
-          animationDuration: const Duration(milliseconds: 800),
-          chartLegendSpacing: 80,
-          chartRadius: 300,
-          initialAngleInDegree: 0,
-          chartType: ChartType.ring,
-          ringStrokeWidth: 35,
-          centerText: "\$9,000",
-          centerTextStyle: TextStyle(
+      body: Column(
+        children: <Widget>[
+          Builder(
+            builder: (BuildContext context) {
+              return ElevatedButton(
+                onPressed: () async {
+                  final resp = await openPopup();
+                  if (resp == null || resp.isEmpty) return;
+                  print(resp);
+                  setState(() {
+                    budgetMap.addAll({actualCategory: double.parse(resp)});
+                    values_added =
+                        true; // Set values_added to true when anything is added
+                  });
+                  print(budgetMap);
+                },
+                child: Text("Add Spending Category"),
+              );
+            },
+          ),
+          SizedBox(height: 24),
+          PieChart(
+            key: UniqueKey(), // Add a unique key to the PieChart widget
+            dataMap: budgetMap,
+            animationDuration: const Duration(milliseconds: 800),
+            chartLegendSpacing: 80,
+            chartRadius: 300,
+            initialAngleInDegree: 0,
+            chartType: ChartType.ring,
+            ringStrokeWidth: 35,
+            centerText: "\$" + getTotalBudget(budgetMap).toString(),
+            centerTextStyle: TextStyle(
               color: color,
               fontWeight: FontWeight.bold,
               fontSize: 40,
@@ -58,18 +97,99 @@ class _GraphPageState extends State<GraphPage> {
                 ..strokeWidth = 25.0
                 ..color = Colors.white
                 ..style = PaintingStyle.stroke
-                ..strokeJoin = StrokeJoin.round),
-          chartValuesOptions: const ChartValuesOptions(
-            showChartValueBackground: false,
-            showChartValues: true,
-            showChartValuesInPercentage: true,
-            // showChartValuesOutside: true,
-            decimalPlaces: 0,
+                ..strokeJoin = StrokeJoin.round,
+            ),
+            chartValuesOptions: ChartValuesOptions(
+              showChartValueBackground: false,
+              showChartValues:
+                  values_added, // Show values when anything is added
+              showChartValuesInPercentage: true,
+              decimalPlaces: 0,
+            ),
+            legendOptions: values_added
+                ? LegendOptions(
+                    showLegends: true,
+                    legendPosition: LegendPosition.right,
+                    legendTextStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                    ),
+                  )
+                : LegendOptions(
+                    showLegends: false), // Hide legends when nothing is added
+            baseChartColor: Colors.white,
+            colorList: colorList,
           ),
-          baseChartColor: Colors.white,
-          colorList: colorList,
-        ),
+        ],
       ),
     );
+  }
+
+  Future<String?> openPopup() => showDialog<String>(
+        context: scaffoldKey.currentContext!,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text("Enter Expense:"),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                DropdownButtonFormField<String>(
+                  value: selectedCategory,
+                  items: [
+                    "Select Category",
+                    "Housing",
+                    "Utilities",
+                    "Food",
+                    "Transportation",
+                    "Entertainment",
+                    "Investments",
+                    "Debt Payments"
+                  ].map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      selectedCategory = newValue!;
+                      isFormValid =
+                          newValue != "Select Category"; // Update form validity
+                    });
+                  },
+                ),
+                SizedBox(height: 10), // Add some spacing
+                TextField(
+                  autofocus: true,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter your expense amount (i.e. \$25)',
+                  ),
+                  controller: controller,
+                ),
+              ],
+            ),
+            actions: [
+              TextButton(onPressed: submit, child: const Text("Submit"))
+            ],
+          );
+        },
+      );
+
+  void submit() {
+    actualCategory = selectedCategory;
+    if (selectedCategory != "Select Category") {
+      budgetMap.addAll({selectedCategory: double.parse(controller.text)});
+      values_added = true; // Set values_added to true when anything is added
+      Navigator.of(context).pop(controller.text);
+      selectedCategory = "Select Category";
+    }
+    controller.clear();
+  }
+
+  double getTotalBudget(Map<String, double> budgetMap) {
+    return budgetMap.values.fold(0,
+        (double previousValue, double currentValue) {
+      return previousValue + currentValue;
+    });
   }
 }
