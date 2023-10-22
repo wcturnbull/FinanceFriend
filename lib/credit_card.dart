6e5e8cb381c03ad26cbb7c8c28eb715e11cddc03
HenryWellman <hdwellma@purdue.edu>
2023-10-22 14:35:53 -0400

    Added Basic Tips Differentiation and Credit Score Input

--- lib/credit_card.dart
+++ lib/credit_card.dart
@@ -1,6 +1,28 @@
 import 'package:flutter/material.dart';
 
-class CreditCardPage extends StatelessWidget {
+class CreditCardPage extends StatefulWidget {
+  @override
+  _CreditCardPageState createState() => _CreditCardPageState();
+}
+
+class _CreditCardPageState extends State<CreditCardPage> {
+  String creditScoreStatus = ''; // Store the credit score status
+
+  // Function to update the credit score status based on the score
+  void updateCreditScoreStatus(int creditScore) {
+    if (creditScore < 560) {
+      creditScoreStatus = 'Bad';
+    } else if (creditScore >= 560 && creditScore < 670) {
+      creditScoreStatus = 'Fair';
+    } else if (creditScore >= 670 && creditScore < 750) {
+      creditScoreStatus = 'Good';
+    } else {
+      creditScoreStatus = 'Excellent';
+    }
+    // Update the UI with the new credit score status
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,6 +56,20 @@ class CreditCardPage extends StatelessWidget {
               child: Text('Select A Card'),
             ),
             SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return CreditScoreInputDialog(updateCreditScoreStatus);
+                  },
+                );
+              },
+              child: Text('Tips'),
+            ),
+            SizedBox(height: 20),
+            Text(
+                'Credit Score: $creditScoreStatus'), // Display credit score status
           ],
         ),
       ),
@@ -76,6 +112,52 @@ class _CardSelectionDialogState extends State<CardSelectionDialog> {
         ElevatedButton(
           onPressed: () {
             // Handle save button action here
+            Navigator.of(context).pop();
+          },
+          child: Text('Save'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            // Handle close button action here
+            Navigator.of(context).pop();
+          },
+          child: Text('Close'),
+        ),
+      ],
+    );
+  }
+}
+
+class CreditScoreInputDialog extends StatefulWidget {
+  final Function(int) onCreditScoreSaved;
+
+  CreditScoreInputDialog(this.onCreditScoreSaved);
+
+  @override
+  _CreditScoreInputDialogState createState() => _CreditScoreInputDialogState();
+}
+
+class _CreditScoreInputDialogState extends State<CreditScoreInputDialog> {
+  TextEditingController creditScoreController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('Enter Credit Score'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          TextField(
+            controller: creditScoreController,
+            decoration: InputDecoration(labelText: 'Credit Score'),
+          ),
+        ],
+      ),
+      actions: <Widget>[
+        ElevatedButton(
+          onPressed: () {
+            final creditScore = int.tryParse(creditScoreController.text) ?? 0;
+            widget.onCreditScoreSaved(creditScore);
             Navigator.of(context).pop();
           },
           child: Text('Save'),
