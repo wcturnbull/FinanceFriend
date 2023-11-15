a21cfc114ee15221a12c8f1625397bc4ac122f99
HenryWellman <hdwellma@purdue.edu>
2023-11-15 13:26:55 -0500

    Added Initial Fade In Animation To Investments Page

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:financefriend/budget_tracking_widgets/budget.dart';
 import 'package:financefriend/budget_tracking_widgets/budget_colors.dart';
 import 'package:financefriend/ff_appbar.dart';
+import 'package:financefriend/investment_page.dart';
 import 'package:financefriend/profile_picture_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -226,15 +227,88 @@ class _HomePageState extends State<HomePage> {
                     Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
+                        /*
                         ElevatedButton(
                           style: const ButtonStyle(
-                              fixedSize:
-                                  MaterialStatePropertyAll(Size(300.0, 50.0))),
+                            fixedSize:
+                                MaterialStatePropertyAll(Size(300.0, 50.0)),
+                          ),
                           onPressed: () {
-                            Navigator.pushNamed(context, '/investments');
+                            Navigator.push(
+                              context,
+                              PageRouteBuilder(
+                                pageBuilder:
+                                    (context, animation, secondaryAnimation) {
+                                  return InvestmentPage(); // Replace with your actual page widget
+                                },
+                                transitionsBuilder: (context, animation,
+                                    secondaryAnimation, child) {
+                                  const begin = Offset(1.0, 0.0);
+                                  const end = Offset.zero;
+                                  const curve = Curves.easeInOut;
+                                  const duration = Duration(
+                                      milliseconds:
+                                          1000); // Adjust the duration here
+
+                                  var tween = Tween(begin: begin, end: end)
+                                      .chain(CurveTween(curve: curve));
+
+                                  var offsetAnimation = animation.drive(tween);
+
+                                  return SlideTransition(
+                                    position: offsetAnimation,
+                                    child: child,
+                                  );
+                                },
+                                transitionDuration: const Duration(
+                                    milliseconds:
+                                        1000), // Adjust the duration here
+                              ),
+                            );
+                          },
+                          child: const Text('Go to Investment Page'),
+                        ), */
+                        ElevatedButton(
+                          style: const ButtonStyle(
+                            fixedSize:
+                                MaterialStatePropertyAll(Size(300.0, 50.0)),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              PageRouteBuilder(
+                                pageBuilder:
+                                    (context, animation, secondaryAnimation) {
+                                  return InvestmentPage(); // Replace with your actual page widget
+                                },
+                                transitionsBuilder: (context, animation,
+                                    secondaryAnimation, child) {
+                                  const begin = 0.0;
+                                  const end = 1.0;
+                                  const curve = Curves.easeInOut;
+                                  const duration = Duration(
+                                      milliseconds:
+                                          2000); // Adjust the duration here
+
+                                  var tween = Tween(begin: begin, end: end)
+                                      .chain(CurveTween(curve: curve));
+
+                                  var opacityAnimation = animation.drive(tween);
+
+                                  return FadeTransition(
+                                    opacity: opacityAnimation,
+                                    child: child,
+                                  );
+                                },
+                                transitionDuration: const Duration(
+                                    milliseconds:
+                                        2000), // Adjust the duration here
+                              ),
+                            );
                           },
                           child: const Text('Go to Investment Page'),
                         ),
+
                         const SizedBox(height: 16), //spacing
                         ElevatedButton(
                           style: const ButtonStyle(
