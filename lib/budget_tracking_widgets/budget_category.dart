4eb22ec06435bff38d8c4f6dc987ad022d48f554
Wes Turnbull <wturnbul@purdue.edu>
2023-11-17 23:28:52 -0500

    minor adjustment to budget_category

--- lib/budget_tracking_widgets/budget_category.dart
+++ lib/budget_tracking_widgets/budget_category.dart
@@ -39,6 +39,7 @@ class _BudgetCategoryTableState extends State<BudgetCategoryTable> {
     });
 
     String editedCategory = category;
+    String originalCat = category;
 
     showDialog(
       context: context,
@@ -80,10 +81,10 @@ class _BudgetCategoryTableState extends State<BudgetCategoryTable> {
             ),
             ElevatedButton(
                 onPressed: () async {
-                  widget.budget.budgetMap.remove(category);
+                  widget.budget.budgetMap.remove(originalCat);
                   widget.onBudgetUpdate(widget.budget.budgetMap);
                   bool success = await removeBudgetCategory(
-                      widget.budget.budgetName, category);
+                      widget.budget.budgetName, originalCat);
                   if (success) {
                     Navigator.of(context).pop();
                     Navigator.of(context).pop();
