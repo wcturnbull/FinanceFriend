6b033ac4f403d571cc5fbfb0dc7c016652b65303
Wes Turnbull <wturnbul@purdue.edu>
2023-11-25 22:24:57 -0500

    Friend list and adding friends implemented

--- lib/social_page.dart
+++ lib/social_page.dart
@@ -14,41 +14,106 @@ final database = FirebaseDatabase.instanceFor(
 final DatabaseReference reference = database.ref();
 final currentUser = FirebaseAuth.instance.currentUser;
 
-void addUserAsFriend(String name) {
+Future<String?> getUidFromName(String name) async {
+  if (currentUser != null) {
+    DatabaseEvent event = await reference.child('userIndex').once();
+    DataSnapshot snapshot = event.snapshot;
+
+    if (snapshot.value != null) {
+      Map<String, dynamic> userIndex = snapshot.value as Map<String, dynamic>;
+      return userIndex[name];
+    }
+  }
+}
+
+Future<void> addUserAsFriend(String name) async {
   if (currentUser != null) {
     String uid = currentUser!.uid;
 
-    // Check if the user's friends list is initialized
+    DatabaseEvent preEvent = await reference.child('users/$uid').once();
+    DataSnapshot preSnapshot = preEvent.snapshot;
+    if (preSnapshot.value != null) {
+      Map<String, dynamic> userData = preSnapshot.value as Map<String, dynamic>;
+      if (userData.containsKey("friends")) {
+        DatabaseEvent event =
+            await reference.child('users/$uid/friends').once();
+        DataSnapshot snapshot = event.snapshot;
+
+        if (snapshot.value != null) {
+          Map<String, dynamic> friendMap =
+              snapshot.value as Map<String, dynamic>;
+          print(friendMap);
+          friendMap[name] = name;
+          reference.child('users').child(uid).child('friends').set(friendMap);
+        }
+      } else {
+        print("NO FRIENDS YET USER");
+
+        Map<String, String> friendMap = {};
+        friendMap[name] = name;
+        reference.child('users').child(uid).child('friends').push();
+        reference.child('users').child(uid).child('friends').set(friendMap);
+      }
+      print(userData);
+    }
+    String? friendUid = await getUidFromName(name);
+
+    DatabaseEvent preEvent2 = await reference.child('users/$friendUid').once();
+    DataSnapshot preSnapshot2 = preEvent2.snapshot;
+
+    if (preSnapshot2.value != null) {
+      Map<String, dynamic> friendData =
+          preSnapshot2.value as Map<String, dynamic>;
+      print("Friend Data:");
+      print(friendData);
+      if (friendData.containsKey("friends")) {
+        DatabaseEvent event2 =
+            await reference.child('users/${friendUid}/friends').once();
+        DataSnapshot snapshot2 = event2.snapshot;
+        if (snapshot2.value != null) {
+          String? userName = currentUser!.displayName;
+          Map<String, dynamic> friendMap =
+              snapshot2.value as Map<String, dynamic>;
+          friendMap[userName!] = userName;
+          reference
+              .child('users')
+              .child(friendUid!)
+              .child('friends')
+              .set(friendMap);
+        }
+      } else {
+        print("NO FRIENDS YET FRIEND");
+        String? userName = currentUser!.displayName;
+        Map<String, String> friendMap = {};
+        friendMap[userName!] = userName;
+        reference.child('users').child(friendUid!).child('friends').push();
+        reference
+            .child('users')
+            .child(friendUid!)
+            .child('friends')
+            .set(friendMap);
+      }
+    }
+  }
+}
+
+Future<void> removeUserAsFriend(String name) async {
+  if (currentUser != null) {
+    String uid = currentUser!.uid;
+
+    reference.child('users').child(uid).child('friends').child(name).remove();
+    String? friendUid = await getUidFromName(name);
+
+    String userName = currentUser?.displayName as String;
+
+    // List<String> otherFriendMap = [userName];
+
     reference
         .child('users')
-        .child(uid)
+        .child(friendUid!)
         .child('friends')
-        .once()
-        .then((DataSnapshot snapshot) {
-          Map<dynamic, dynamic>? friendsMap =
-              snapshot.value as Map<dynamic, dynamic>?;
-
-          if (friendsMap == null) {
-            // If the friends list is not initialized, initialize it with the new friend
-            friendsMap = {name: true};
-          } else {
-            // Check if the user is not already a friend
-            if (!friendsMap.containsKey(name)) {
-              // If the user is not a friend, add them
-              friendsMap[name] = true;
-            } else {
-              // If the user is already a friend, you can implement the logic to remove them
-              // For now, let's just print a message
-              print("User $name is already a friend.");
-            }
-          }
-
-          // Update the user's friends list in the database
-          reference.child('users').child(uid).child('friends').set(friendsMap);
-        } as FutureOr Function(DatabaseEvent value))
-        .catchError((error) {
-      print("Error: $error");
-    });
+        .child(userName)
+        .remove();
   }
 }
 
@@ -61,6 +126,9 @@ class SocialPage extends StatefulWidget {
 
 class _SocialPageState extends State<SocialPage> {
   List<String> userNames = [];
+  List<String> userFriends = [];
+  Map<String, bool> friendStatus =
+      {}; // Keep track of friend status for each user
 
   @override
   void initState() {
@@ -86,6 +154,33 @@ class _SocialPageState extends State<SocialPage> {
 
         // Update the UI
         setState(() {});
+      }
+    });
+
+    reference
+        .child('users')
+        .child(currentUser!.uid)
+        .child('friends')
+        .onValue
+        .listen((event) {
+      DataSnapshot snapshot = event.snapshot;
+
+      if (snapshot.value is Map) {
+        Map<String, dynamic> friendMap =
+            Map<String, dynamic>.from(snapshot.value as Map<String, dynamic>);
+        userFriends = friendMap.keys.toList();
+
+        print("Current User Friends:");
+        print(userFriends);
+
+        // Update the UI
+        setState(() {
+          // Update friend status for each user
+          friendStatus = {};
+          for (var userName in userNames) {
+            friendStatus[userName] = userFriends.contains(userName);
+          }
+        });
       }
     });
   }
@@ -121,22 +216,23 @@ class _SocialPageState extends State<SocialPage> {
                           children: [
                             Text(userNames[index]),
                             ElevatedButton(
-                              child: Text("Add Friend"),
+                              child: Text(friendStatus[userNames[index]] == true
+                                  ? "Remove Friend"
+                                  : "Add Friend"),
                               onPressed: () {
                                 // Handle the button click here
                                 // You can add the logic to do something when the button is clicked
-                                addUserAsFriend(userNames[index]);
-                                print(
-                                    "Add button clicked for ${userNames[index]}");
-                                // Optionally, you can update the UI to reflect the change
+                                if (friendStatus[userNames[index]] == true) {
+                                  // User is already a friend, so remove
+                                  removeUserAsFriend(userNames[index]);
+                                } else {
+                                  // User is not a friend, so add
+                                  addUserAsFriend(userNames[index]);
+                                }
                                 setState(() {
-                                  // Update the button label to "Remove Friend" (not functional yet)
-                                  // You may need to modify this logic based on whether the user is already a friend
-                                  // For simplicity, this example assumes the user is not already a friend
-                                  // If the user is already a friend, you can set the button label accordingly
-                                  // For example, you could maintain a list of friends and check if the user is in that list
-                                  // and set the button label accordingly.
-                                  userNames[index] = "Remove Friend";
+                                  // Update the friend status for the clicked user
+                                  friendStatus[userNames[index]] =
+                                      !friendStatus[userNames[index]]!;
                                 });
                               },
                             ),
